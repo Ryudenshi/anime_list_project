b5f143918088d025d061a9e641f67fac10d18a5d
Ryudenshi <skarydog17@gmail.com>
2023-08-01 10:58:04 +0300

    anime_genres model added

--- app/Models/Anime_Genre.php
+++ app/Models/Anime_Genre.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Anime_Genre extends Model
 {
     use HasFactory;
+
+    protected $table = 'anime_genre';
+
+    protected $primaryKey =['anime_id', 'genre_id'];
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'anime_id',
+        'genre_id',
+    ];
+
+    public function anime()
+    {
+        return $this->belongsTo(Anime::class, 'anime_id', 'id');
+    }
+
+    public function genres()
+    {
+        return $this->belongsTo(Genre::class, 'genre_id', 'id');
+    }
 }
